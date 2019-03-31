d4132e61559587d66aa96af25578d9247b20adc9
yonidavidson <yoni.davidson@tg-17.com>
2019-03-31 17:18:15 +0300

    build example

--- lib/carrow_lib.go
+++ lib/carrow_lib.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"C"
+	"fmt"
 
 	"github.com/353solutions/carrow"
 )
 
-//export Mock
-func Mock() {
+//export Build
+func Build() {
 	size := 100
 	intBld := carrow.NewInt64ArrayBuilder()
 	floatBld := carrow.NewFloat64ArrayBuilder()
@@ -16,6 +17,51 @@ func Mock() {
 		floatBld.Append(float64(i))
 	}
 
+	intArr, err := intBld.Finish()
+	if err != nil {
+		fmt.Printf("intBld error: %s", err)
+		return
+	}
+
+	floatArr, err := floatBld.Finish()
+	if err != nil {
+		fmt.Printf("floatBld error: %s", err)
+		return
+	}
+
+	intField, err := carrow.NewField("incCol", carrow.Integer64Type)
+	if err != nil {
+		fmt.Printf("intField error: %s", err)
+		return
+	}
+
+	floatField, err := carrow.NewField("floatCol", carrow.Float64Type)
+	if err != nil {
+		fmt.Printf("floatField error: %s", err)
+		return
+	}
+
+	intCol, err := carrow.NewColumn(intField, intArr)
+	if err != nil {
+		fmt.Printf("intCol error: %s", err)
+		return
+	}
+
+	floatCol, err := carrow.NewColumn(floatField, floatArr)
+	if err != nil {
+		fmt.Printf("floatCol error: %s", err)
+		return
+	}
+
+	cols := []*carrow.Column{intCol, floatCol}
+	table, err := carrow.NewTableFromColumns(cols)
+	if err != nil {
+		fmt.Printf("table creation error: %s", err)
+		return
+	}
+
+	fmt.Printf("num cols: %d\n", table.NumCols())
+	fmt.Printf("num rows: %d\n", table.NumRows())
 }
 
 func main() {}
