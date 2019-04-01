041204eea38ef2efc8bb4216fcbdc9e4a37c9161
yonidavidson <yoni.davidson@tg-17.com>
2019-04-01 08:08:17 +0300

    do python bindings

---
+++ python-bindings/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+import os
+import numpy as np
+import pyarrow as pa
+
+
+ext_modules = cythonize("example.pyx")
+
+for ext in ext_modules:
+    # The Numpy C headers are currently required
+    ext.include_dirs.append(np.get_include())
+    ext.include_dirs.append(pa.get_include())
+    ext.libraries.extend(pa.get_libraries())
+    ext.library_dirs.extend(pa.get_library_dirs())
+
+    if os.name == 'posix':
+        ext.extra_compile_args.append('-std=c++11')
+
+    # Try uncommenting the following line on Linux
+    # if you get weird linker errors or runtime crashes
+    # ext.define_macros.append(("_GLIBCXX_USE_CXX11_ABI", "0"))
+
+
+setup(ext_modules=ext_modules)
