c92a527ddc0cea89c77a1c895c657d1c78829222
yonidavidson <yoni.davidson@tg-17.com>
2019-04-16 07:58:09 +0300

    get shared pointer

--- py/bindings.pyx
+++ py/bindings.pyx
@@ -2,15 +2,19 @@
 # cython: language_level=3
 
 from pyarrow.lib cimport *
+from cython.operator cimport dereference as deref
 cimport c_bindings
 
 def try_build():
     table = c_bindings.CreateTable()
     l = callArrow(table)
-    print(l)
+    #print(l)
+    return l
 
 cdef callArrow(void* table):
-    cdef shared_ptr[CTable]* table_pointer = <shared_ptr[CTable]*>table
+    cdef const shared_ptr[CTable]* table_pointer = <shared_ptr[CTable]*>table
     if table_pointer == NULL:
-        raise TypeError("not a table")
-    return (table_pointer.get().num_columns(),table_pointer.get().num_rows())
+        raise TypeError("pointer is not a table")
+    cdef shared_ptr[CTable] t = deref(table_pointer)
+    print("bindings table: rows:",t.get().num_rows(),"columns:",t.get().num_columns())
+    return pyarrow_wrap_table(t)
